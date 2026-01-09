595962b619921dd0f26cc43fddc9cac82e7a6ddf
doprz <52579214+doprz@users.noreply.github.com>
2026-01-09 13:08:28 -0600

    feat: init

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Ziglings nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    zig.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          zig = inputs.zig.packages.${system}."0.15.2";
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                inherit zig;
+              })
+            ];
+            config = { };
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "zig_life-dev";
+
+            packages = [
+              zig
+              pkgs.zls
+            ];
+          };
+        };
+    };
+}
